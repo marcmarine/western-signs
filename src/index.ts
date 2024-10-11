a37585f280145b41292e478ac5f56408aa143485
Marc Mariné <shenobi@gmail.com>
2024-10-11 09:56:25 +0200

    chore(docs): Set up TypeDoc for generating API docs

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export { getSignByDate } from './getSignByDate'
 export { getSigns } from './getSigns'
 
 export { SIGNS, ELEMENTS, MODALITIES, PLANETS } from '@/data/constants'
+
+export type { Sign, Signs, Planets, Modalities, Elements } from './definitions'
