76cd34576ba56941fe5d19c454e5b5919cc3fdfb
Marc Mariné <shenobi@gmail.com>
2025-07-29 15:18:14 +0200

    docs: Add JSDoc for getPlanet function

--- src/getPlanet.ts
+++ src/getPlanet.ts
@@ -2,6 +2,26 @@ import { dictionaries } from '../data/dictionaries'
 import planets from '../data/planets'
 import type { Dictionary, Language, Planet, Planets } from './definitions'
 
+/**
+ * Get a single planet with its translated properties for a specified language.
+ *
+ * @param {Planets} planetKey - The key corresponding to the desired planet.
+ * @param {Language} [language='en'] - The language code to use for translations. Defaults to 'en'.
+ * @returns {Planet | null} An object with translated values based on the specified language, or null  if the planet key is invalid.
+ *
+ * @example
+ * import { getPlanet, PLANETS } from 'western-signs';
+ *
+ * // Retrieve information about Mars in English
+ * const mars = getPlanet(PLANETS.MARS);
+ * console.log(mars);
+ * // Output:
+ * // {
+ * //   name: 'Mars',
+ * //   glyph: '♂',
+ * //   type: 'Personal'
+ * // }
+ */
 export function getPlanet(
   planetKey: Planets,
   language: Language = 'en',
