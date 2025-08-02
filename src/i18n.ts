ece47b5f6af844e440099f2ec07bd48a5ef7630d
Marc Mariné <shenobi@gmail.com>
2025-08-02 19:41:13 +0200

    docs: Add JSDoc comments to i18n language functions

--- src/i18n.ts
+++ src/i18n.ts
@@ -3,14 +3,37 @@ import type { Dictionary, Language } from './definitions'
 
 let currentLanguage: Language = 'es'
 
+/**
+ * Sets the current language to the provided language.
+ * @param {Language} language - The language to set as current.
+ */
 export function setLanguage(language: Language): void {
   currentLanguage = language
 }
 
+/**
+ * Retrieves the current language.
+ * @returns {Language} The current language.
+ */
 export function getLanguage(): Language {
   return currentLanguage
 }
 
+/**
+ * Translates template strings into the current language using the provided values.
+ * @param {TemplateStringsArray} strings - The template strings to be translated.
+ * @param {string[]} values - The values to be translated within the template.
+ * @returns {string} The translated string.
+ *
+ * @example
+ * import { SIGNS, i18n } from 'western-signs'
+ *
+ * // The assigned default language is Spanish.
+ * const taurus = i18n.t`${SIGNS.TAURUS}`
+ * console.log(taurus)
+ * // Output: Tauro
+ *
+ */
 export function t(strings: TemplateStringsArray, ...values: string[]): string {
   const language = getLanguage()
 
