a5083f04cddb544a44a9deb7b8d80b8f1c7ac4e0
Marc Mariné <shenobi@gmail.com>
2025-08-09 14:34:23 +0200

    feat: Refactor getSymbol to support method chaining

--- src/getSymbol.ts
+++ src/getSymbol.ts
@@ -4,7 +4,6 @@ import { fileURLToPath } from 'node:url'
 import type { Symbols } from './definitions'
 
 export interface SymbolOptions {
-  dataUrl?: boolean
   stroke?: string
   strokeWidth?: number | string
 }
@@ -14,13 +13,26 @@ export interface SymbolOptions {
  * @param {Symbols} symbolName Name of the icon without extension .svg
  * @param {SymbolOptions} options Options to modify attributes or return as a Data URL
  * @returns SVG as string or Data URL
+ *
+ * @example
+ * import { getSymbol } from 'western-signs'
+ *
+ * // Get the symbol with initial options
+ * const initialSymbol = getSymbol('taurus', { stroke: 'blue', strokeWidth: 2 })
+ * // Modify attributes using method chaining
+ * const modifiedSymbol = initialSymbol.setStroke('green').setStrokeWidth(3)
+ *
+ * // Log the modified SVG as a string
+ * console.log(symbol.toString())
+ *
+ * // Log the modified SVG as a Data URL
+ * console.log(symbol.toDataURL())
+ *
+ * @throws Will throw an error if the specified SVG file is not found
  */
-export function getSymbol(
-  symbolName: Symbols,
-  options: SymbolOptions = {},
-): string | undefined {
+export function getSymbol(symbolName: Symbols, options: SymbolOptions = {}) {
   if (typeof window !== 'undefined') {
-    return
+    return undefined
   }
 
   const __dirname = path.dirname(fileURLToPath(import.meta.url))
@@ -30,26 +42,53 @@ export function getSymbol(
     throw new Error(`Icon "${symbolName}" not found.`)
   }
 
-  let svgString = readFileSync(iconPath, 'utf8')
+  const svgString = readFileSync(iconPath, 'utf8')
 
-  if (options.stroke) {
-    svgString = svgString.replace(
-      /stroke="[^"]*"/g,
-      `stroke="${options.stroke}"`,
-    )
+  return createSymbol(svgString, options)
+}
+
+function createSymbol(svgString: string, options: SymbolOptions = {}) {
+  let currentStroke = options.stroke
+  let currentStrokeWidth = options.strokeWidth
+
+  const setStroke = (stroke: string) => {
+    currentStroke = stroke
+    return api
   }
 
-  if (options.strokeWidth) {
-    svgString = svgString.replace(
-      /stroke-width="[^"]*"/g,
-      `stroke-width="${Number(options.strokeWidth)}"`,
-    )
+  const setStrokeWidth = (strokeWidth: number | string) => {
+    currentStrokeWidth = strokeWidth
+    return api
   }
 
-  if (options.dataUrl) {
-    const base64 = Buffer.from(svgString).toString('base64')
+  const toString = (): string => {
+    let modifiedSvgString = svgString
+    if (currentStroke) {
+      modifiedSvgString = modifiedSvgString.replace(
+        /stroke="[^"]*"/g,
+        `stroke="${currentStroke}"`,
+      )
+    }
+    if (currentStrokeWidth) {
+      modifiedSvgString = modifiedSvgString.replace(
+        /stroke-width="[^"]*"/g,
+        `stroke-width="${currentStrokeWidth}"`,
+      )
+    }
+    return modifiedSvgString
+  }
+
+  const toDataURL = (): string => {
+    const base64 = Buffer.from(toString()).toString('base64')
     return `data:image/svg+xml;base64,${base64}`
   }
 
-  return svgString
+  const api = {
+    setStroke,
+    setStrokeWidth,
+    toString,
+    toDataURL,
+  }
+
+  return api
 }
--- tests/getSymbol.test.ts
+++ tests/getSymbol.test.ts
@@ -14,7 +14,8 @@ const symbolNames = [
 describe('getSymbol', () => {
   describe.each(symbolNames)('should read an SVG as string for', symbolName => {
     it(`${capitalizeString(symbolName)}`, () => {
-      const svgString = getSymbol(symbolName as Symbols)
+      const symbol = getSymbol(symbolName as Symbols)
+      const svgString = symbol?.toString()
 
       expect(svgString).toContain('<svg')
       expect(svgString).toContain('stroke="currentColor"')
@@ -22,27 +23,42 @@ describe('getSymbol', () => {
     })
   })
 
-  test('should modify stroke and stroke-width', () => {
+  test('should modify stroke and stroke-width correctly', () => {
     symbolNames.forEach(symbolName => {
-      const svgString = getSymbol(symbolName as Symbols, {
+      const options = {
         stroke: 'red',
         strokeWidth: '3',
-      })
+      }
+      const symbol = getSymbol(symbolName as Symbols, options)
 
-      expect(svgString).toContain('stroke="red"')
-      expect(svgString).toContain('stroke-width="3"')
+      const svgString = symbol?.toString()
+
+      expect(svgString).toContain(`stroke="${options.stroke}"`)
+      expect(svgString).toContain(`stroke-width="${options.strokeWidth}"`)
     })
   })
 
   test('should return as Data URL (base64)', () => {
-    const dataUrl = getSymbol('taurus', { dataUrl: true }) as string
+    const symbol = getSymbol('taurus')
 
-    expect(dataUrl.startsWith('data:image/svg+xml;base64,')).toBe(true)
+    const dataUrl = symbol?.toDataURL()
+
+    expect(dataUrl?.startsWith('data:image/svg+xml;base64,')).toBe(true)
   })
 
   test('should throw an error if the file does not exist', () => {
     expect(() => getSymbol('notexist' as Symbols)).toThrow(
       'Icon "notexist" not found.',
     )
+  })
+
+  test('should allow for method chaining', () => {
+    const symbol = getSymbol('taurus')
+    const modifiedSymbol = symbol?.setStroke('green').setStrokeWidth('2')
+
+    const svgString = modifiedSymbol?.toString()
+
+    expect(svgString).toContain('stroke="green"')
+    expect(svgString).toContain('stroke-width="2"')
   })
 })
