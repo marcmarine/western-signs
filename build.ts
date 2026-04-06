2f25fc2f2989b446bda7f2cf1fcb540d839e401e
Marc Mariné <shenobi@gmail.com>
2026-04-06 17:10:10 +0200

    chore: Update build to use dts-bundle-generator

--- build.ts
+++ build.ts
@@ -1,10 +1,7 @@
-import dts from 'bun-plugin-dts'
-
 await Bun.build({
   entrypoints: ['./src/index.ts'],
   format: 'esm',
   minify: true,
   outdir: './dist',
-  plugins: [dts()],
   target: 'node'
 })
